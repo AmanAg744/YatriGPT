500f99e55400c864c8f070fbc40841ff2d81f547
AnirudhA124 <118988859+AnirudhA124@users.noreply.github.com>
2023-12-23 23:26:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, request, render_template,url_for,redirect
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
-
+import json
+from ast import literal_eval
 
 # Replace the placeholder with your Atlas connection string
 uri = "mongodb+srv://AnirudhAgrawal1244:Anirudh%40124@anirudhscluster.ccvhnqb.mongodb.net/"
@@ -25,6 +26,8 @@ def signup_get():
         email=request.form["email"]
         password=request.form["password"]
         confirmPassword=request.form["confirmpassword"]
+        cred_list=[[username,email,password,confirmPassword]]
+        print(cred_list)
         print("Email is:",email)
         print("Password is:",password)
         print("Confirm Password is:",confirmPassword)
@@ -37,15 +40,8 @@ def signup_get():
         elif existing_user:
             return f"The user with emailid:{email} exists"
         elif password==confirmPassword:
-            db=client["YatriGPT"]
-            collection=db["UserInfo"]
-            credential={"name":f"{username}","email":f"{email}","password":f"{password}"}
-            result=collection.insert_one(credential)
-            if result.inserted_id:
-                print(f"Data inserted successfully. Inserted ID: {result.inserted_id}")
-            else:
-                print("Failed to inser data")            
-        return redirect(url_for("index"))
+            return redirect(url_for("travel_type", cred_list=cred_list))            
+        return redirect(url_for("travel_type"))
     return "Failure"
 
 @app.route('/login', methods=['POST'])
@@ -67,5 +63,52 @@ def login():
             return "user not found please signup"
     return "Failure"
 
+def convert_string_to_list(string_list):
+    try:
+        # Using literal_eval to safely evaluate the string as a Python expression
+        converted_list = literal_eval(string_list)
+        if isinstance(converted_list, list):
+            return converted_list
+        else:
+            return []
+    except (ValueError, SyntaxError) as e:
+        print(f"Error converting string to list: {e}")
+        return []
+
+@app.route('/travel_type')
+def travel_type():
+    cred_list = request.args.get("cred_list")
+    print(cred_list)
+    cred_list=convert_string_to_list(cred_list)
+    print(type(cred_list))
+    print("Name: ", cred_list[0])
+    print("Email: ", cred_list[1])
+    print("Password: ", cred_list[2])
+    print("Confirm Password: ", cred_list[3])
+    return render_template('buttons.html', cred_list=cred_list)
+
+@app.route('/button_clicked', methods=['POST'])
+def button_clicked():
+    button_value = request.form['button_value']
+    cred_list = request.form['cred_list']
+    print(type(cred_list))
+    print(cred_list)
+    cred_list=convert_string_to_list(cred_list)
+    print(type(cred_list))
+    print(cred_list[0])
+    username=cred_list[0]
+    email=cred_list[1]
+    password=cred_list[2]
+    confirmPassword=cred_list[3]
+    travel_type=button_value
+    db=client["YatriGPT"]
+    collection=db["UserInfo"]
+    credential={"name":f"{username}","email":f"{email}","password":f"{password}","travel-type":f"{travel_type}"}
+    result=collection.insert_one(credential)
+    if result.inserted_id:
+        print(f"Data inserted successfully. Inserted ID: {result.inserted_id}")
+    else:
+        print("Failed to inser data")
+    return f"The clicked button value is: {button_value}, Cred list: {cred_list}"
 if __name__ == '__main__':
     app.run(debug=True)
