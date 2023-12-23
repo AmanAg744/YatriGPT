34b15a5b8c51def425d32823327642620cd5eec5
AnirudhA124 <118988859+AnirudhA124@users.noreply.github.com>
2023-12-23 19:44:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
 from flask import Flask, request, render_template,url_for,redirect
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
 
+
+# Replace the placeholder with your Atlas connection string
+uri = "mongodb+srv://AnirudhAgrawal1244:Anirudh%40124@anirudhscluster.ccvhnqb.mongodb.net/"
+# Set the Stable API version when creating a new client
+client = MongoClient(uri, server_api=ServerApi('1'))
+                          
 app = Flask(__name__)
 
 @app.route('/')
@@ -13,14 +21,30 @@ def signup():
 @app.route('/signup_get', methods=['POST'])
 def signup_get():
     if request.method == "POST":
+        username=request.form["username"]
         email=request.form["email"]
         password=request.form["password"]
         confirmPassword=request.form["confirmpassword"]
         print("Email is:",email)
         print("Password is:",password)
         print("Confirm Password is:",confirmPassword)
-        if password!=confirmPassword:
-            return "Password is not equal to Confirm Password"
+        db=client["YatriGPT"]
+        collection=db["UserInfo"]
+        query = {"email": email}
+        existing_user=collection.find_one(query)
+        if email=="" or password =="" or confirmPassword=="" or username=="":
+            return "fields are left empty"
+        elif existing_user:
+            return f"The user with emailid:{email} exists"
+        elif password==confirmPassword:
+            db=client["YatriGPT"]
+            collection=db["UserInfo"]
+            credential={"name":f"{username}","email":f"{email}","password":f"{password}"}
+            result=collection.insert_one(credential)
+            if result.inserted_id:
+                print(f"Data inserted successfully. Inserted ID: {result.inserted_id}")
+            else:
+                print("Failed to inser data")            
         return redirect(url_for("index"))
     return "Failure"
 
@@ -29,9 +53,18 @@ def login():
     if request.method == "POST":
         email=request.form["email"]
         password=request.form["password"]
-        print(email)
-        print(password)
-        return "Success"
+        db=client["YatriGPT"]
+        collection=db["UserInfo"]
+        cred_query = {"email": email,"password":password}
+        cred_found=collection.find_one(cred_query)
+        if email=="" or password =="":
+            return "fields are left empty"
+        elif cred_found:
+            print(cred_found["name"])
+            user_name=cred_found["name"]
+            return f"Welcome {user_name}"
+        else:
+            return "user not found please signup"
     return "Failure"
 
 if __name__ == '__main__':
